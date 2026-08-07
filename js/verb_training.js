2d2f7f382cbdaf1b214b41320ef666e348123a6e
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:51:31 +0300

    Create verb_training.js

---
+++ js/verb_training.js
@@ -0,0 +1,57 @@
+const verbs=[
+
+
+{
+
+verb:"gitmek",
+
+zh:"去",
+
+example:
+
+"İşe gidiyorum."
+
+},
+
+
+{
+
+verb:"gelmek",
+
+zh:"來",
+
+example:
+
+"Eve geliyorum."
+
+},
+
+
+{
+
+verb:"istemek",
+
+zh:"想要",
+
+example:
+
+"Su istiyorum."
+
+},
+
+
+{
+
+verb:"yemek",
+
+zh:"吃",
+
+example:
+
+"Yemek yiyorum."
+
+}
+
+
+
+];
