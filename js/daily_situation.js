d07f0eb20761d4eea05b0ec68071261ddd58c16e
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:09:33 +0300

    Create daily_situation.js

---
+++ js/daily_situation.js
@@ -0,0 +1,144 @@
+const survivalSituations=[
+
+
+{
+
+title:"🏦 銀行開戶",
+
+need:[
+
+"護照",
+
+"居留文件",
+
+"地址證明"
+
+],
+
+
+sentence:[
+
+{
+
+tr:"Hesap açmak istiyorum.",
+
+zh:"我想開戶。"
+
+},
+
+{
+
+tr:"Hangi belgeler gerekli?",
+
+zh:"需要哪些文件？"
+
+}
+
+]
+
+},
+
+
+
+
+{
+
+title:"📱 辦手機門號",
+
+need:[
+
+"護照",
+
+"居留證"
+
+],
+
+
+sentence:[
+
+{
+
+tr:"Yeni hat almak istiyorum.",
+
+zh:"我想申請新門號。"
+
+},
+
+{
+
+tr:"İnternet paketi var mı?",
+
+zh:"有網路方案嗎？"
+
+}
+
+]
+
+},
+
+
+
+
+{
+
+title:"🏥 去藥局",
+
+need:[
+
+"描述症狀"
+
+],
+
+sentence:[
+
+{
+
+tr:"Başım ağrıyor.",
+
+zh:"我頭痛。"
+
+},
+
+{
+
+tr:"İlaç önerir misiniz?",
+
+zh:"可以推薦藥嗎？"
+
+}
+
+]
+
+},
+
+
+
+
+{
+
+title:"🚇 搭Metro",
+
+sentence:[
+
+{
+
+tr:"Metro nereden geçiyor?",
+
+zh:"Metro從哪裡經過？"
+
+},
+
+{
+
+tr:"Kart nereden alabilirim?",
+
+zh:"哪裡可以買交通卡？"
+
+}
+
+]
+
+}
+
+
+];
