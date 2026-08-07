7042d0dbd6ed665c83e782daa9cd651916c289d1
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:47:48 +0300

    Create neighbor_mode.js

---
+++ js/neighbor_mode.js
@@ -0,0 +1,63 @@
+const neighborDialogue=[
+
+
+{
+
+tr:
+"Hoş geldiniz.",
+
+zh:
+"歡迎來到這裡。",
+
+reply:
+"Hoş bulduk."
+
+},
+
+
+
+{
+
+tr:
+"Komşunuz muyum?",
+
+zh:
+"我是你的鄰居嗎？",
+
+reply:
+"Evet, memnun oldum."
+
+},
+
+
+
+{
+
+tr:
+"Nerelisiniz?",
+
+zh:
+"您是哪裡人？",
+
+reply:
+"Ben Tayvanlıyım."
+
+},
+
+
+
+{
+
+tr:
+"Türkiye'ye hoş geldiniz.",
+
+zh:
+"歡迎來土耳其。",
+
+reply:
+"Teşekkür ederim."
+
+}
+
+
+];
