319c4e0eb664fca9510ea4ec0fceffaf333ef5a3
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 15:01:58 +0300

    Create grammar_database.js

---
+++ js/grammar_database.js
@@ -0,0 +1,138 @@
+const grammarDatabase=[
+
+
+{
+
+id:1,
+
+title:"人稱代名詞",
+
+turkish:
+"Kişi Zamirleri",
+
+level:"A1",
+
+rule:
+
+"Ben=我, Sen=你, O=他/她, Biz=我們, Siz=您/你們, Onlar=他們",
+
+examples:[
+
+"Ben Eva'yım.",
+"Ben Tayvanlıyım."
+
+]
+
+},
+
+
+
+{
+
+id:2,
+
+title:"名詞句 是/不是",
+
+turkish:
+"Olumlu Olumsuz Cümle",
+
+level:"A1",
+
+rule:
+
+"名詞後加入人稱字尾表示是，否定使用 değil",
+
+examples:[
+
+"Ben öğrenciyim. (我是學生)",
+
+"Ben öğrenci değilim. (我不是學生)"
+
+]
+
+},
+
+
+
+{
+
+id:3,
+
+title:"現在進行式",
+
+turkish:
+"Şimdiki Zaman",
+
+level:"A1",
+
+rule:
+
+"動詞詞幹 + (I)yor + 人稱",
+
+examples:[
+
+"Geliyorum. 我正在來",
+
+"Çalışıyorum. 我正在工作"
+
+]
+
+},
+
+
+
+{
+
+id:4,
+
+title:"疑問句",
+
+turkish:
+"Soru Cümlesi",
+
+level:"A1",
+
+rule:
+
+"加入疑問詞 mi/mı/mu/mü",
+
+examples:[
+
+"Geliyor musun?",
+
+"Türkçe biliyor musun?"
+
+]
+
+},
+
+
+
+{
+
+id:5,
+
+title:"否定句",
+
+turkish:
+"Olumsuz",
+
+level:"A1",
+
+rule:
+
+"動詞加入 -ma/-me",
+
+examples:[
+
+"Gelmedim.",
+
+"Anlamıyorum."
+
+]
+
+}
+
+
+
+];
