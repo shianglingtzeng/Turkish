9e2549aebaf6bf4075262212d065a824338cb871
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 10:52:56 +0300

    Update dialogue_database.js

--- dialogue_database.js
+++ dialogue_database.js
@@ -1,1 +1,64 @@
+const dialogues=[
 
+
+{
+
+scene:"🛒 超市",
+
+turkish:
+"Poşet ister misiniz?",
+
+chinese:
+"需要袋子嗎？",
+
+answer:
+"Evet, lütfen.",
+
+answerChinese:
+"好的，謝謝"
+
+},
+
+
+
+{
+
+scene:"☕ 咖啡店",
+
+turkish:
+"Ne istersiniz?",
+
+chinese:
+"您想要什麼？",
+
+answer:
+"Bir kahve lütfen.",
+
+answerChinese:
+"一杯咖啡"
+
+},
+
+
+
+{
+
+scene:"🏠 房東",
+
+turkish:
+"Aidat ödendi mi?",
+
+chinese:
+"管理費付了嗎？",
+
+answer:
+"Evet, ödendi.",
+
+answerChinese:
+"已付款"
+
+}
+
+
+
+];
