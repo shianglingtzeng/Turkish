cb6c02e6e9e7fcf0ae3f094189b5cd8158910323
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:49:05 +0300

    Create a1_curriculum.js

---
+++ js/a1_curriculum.js
@@ -0,0 +1,129 @@
+const A1Course=[
+
+
+{
+
+day:1,
+
+title:"自我介紹",
+
+grammar:
+"Ben / Sen",
+
+words:[
+
+"Merhaba 你好",
+
+"Ben 我",
+
+"Ad 我的名字",
+
+"Türkiye 土耳其"
+
+],
+
+mission:
+
+"完成30秒自我介紹"
+
+},
+
+
+
+{
+
+day:2,
+
+title:"國籍與語言",
+
+grammar:
+"Ben ...liyim",
+
+words:[
+
+"Tayvanlı 台灣人",
+
+"Türk 土耳其人",
+
+"İngilizce 英文",
+
+"Türkçe 土耳其語"
+
+]
+
+},
+
+
+
+{
+
+day:3,
+
+title:"數字",
+
+grammar:
+"數字+名詞",
+
+words:[
+
+"bir 一",
+
+"iki 二",
+
+"üç 三",
+
+"on 十"
+
+]
+
+},
+
+
+
+{
+
+day:4,
+
+title:"家庭",
+
+grammar:
+"我的 / 你的",
+
+words:[
+
+"anne 媽媽",
+
+"baba 爸爸",
+
+"aile 家庭"
+
+]
+
+},
+
+
+
+{
+
+day:5,
+
+title:"日常生活",
+
+grammar:
+"現在式",
+
+words:[
+
+"gitmek 去",
+
+"gelmek 來",
+
+"yemek 吃"
+
+]
+
+}
+
+
+
+];
