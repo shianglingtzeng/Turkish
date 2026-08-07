c4ab14a8bc7a9cd75355f73a862e0d5a0fd3497d
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:55:51 +0300

    Update speech_engine.js

--- js/speech_engine.js
+++ js/speech_engine.js
@@ -1,32 +1,25 @@
-/*
-=================================
- Turkish Speech Engine
-=================================
-*/
-
-
 function speakTurkish(text){
 
 
-let speech = new SpeechSynthesisUtterance();
+let speech=
 
+new SpeechSynthesisUtterance(text);
 
-speech.text=text;
 
 
 speech.lang="tr-TR";
 
 
-speech.rate=0.8;
+speech.rate=0.75;
 
 
 speech.pitch=1;
 
 
 
-window.speechSynthesis.speak(
-speech
-);
+window
+.speechSynthesis
+.speak(speech);
 
 
 }
