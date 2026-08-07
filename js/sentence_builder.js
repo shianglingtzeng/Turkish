fca77ae058cac20fa576cddd1149201624cffc39
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 12:14:36 +0300

    Create sentence_builder.js

---
+++ js/sentence_builder.js
@@ -0,0 +1,51 @@
+function buildSentence(action,item){
+
+
+let sentence="";
+
+
+
+if(
+action==="買"
+){
+
+sentence=
+
+"Ben "
+
++
+
+item
+
++
+
+" almak istiyorum.";
+
+}
+
+
+
+if(
+action==="想要"
+){
+
+sentence=
+
+"Ben "
+
++
+
+item
+
++
+
+" istiyorum.";
+
+}
+
+
+
+return sentence;
+
+
+}
