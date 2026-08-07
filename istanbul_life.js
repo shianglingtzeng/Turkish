c12b7bccbf12e9d05dfe6164bdc6fe4501d6a5cc
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 11:36:15 +0300

    Create istanbul_life.js

---
+++ istanbul_life.js
@@ -0,0 +1,59 @@
+const istanbulLife=[
+
+
+{
+
+place:"Esentepe",
+
+items:[
+
+"🏠 Aidat 管理費",
+
+"📄 Sözleşme 合約",
+
+"🔧 Tamir 修理",
+
+"🚇 Metro"
+
+]
+
+},
+
+
+
+{
+
+place:"Şişli",
+
+items:[
+
+"🛒 Migros 超市",
+
+"💊 Eczane 藥局",
+
+"☕ Kahve 咖啡"
+
+]
+
+},
+
+
+
+{
+
+place:"Maslak",
+
+items:[
+
+"🏢 Plaza",
+
+"👔 Toplantı",
+
+"🔐 Güvenlik"
+
+]
+
+}
+
+
+];
