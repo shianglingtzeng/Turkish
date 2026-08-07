68052655cb0e4fb1dd2f310cb67769f1a9b31933
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 15:04:29 +0300

    Create verb_database.js

---
+++ js/verb_database.js
@@ -0,0 +1,78 @@
+const verbDatabase = [
+
+{
+    infinitive:"gelmek",
+    root:"gel",
+    zh:"來",
+
+    present:{
+        ben:"geliyorum",
+        sen:"geliyorsun",
+        o:"geliyor",
+        biz:"geliyoruz",
+        siz:"geliyorsunuz",
+        onlar:"geliyorlar"
+    }
+},
+
+{
+    infinitive:"gitmek",
+    root:"git",
+    zh:"去",
+
+    present:{
+        ben:"gidiyorum",
+        sen:"gidiyorsun",
+        o:"gidiyor",
+        biz:"gidiyoruz",
+        siz:"gidiyorsunuz",
+        onlar:"gidiyorlar"
+    }
+},
+
+{
+    infinitive:"yemek",
+    root:"ye",
+    zh:"吃",
+
+    present:{
+        ben:"yiyorum",
+        sen:"yiyorsun",
+        o:"yiyor",
+        biz:"yiyoruz",
+        siz:"yiyorsunuz",
+        onlar:"yiyorlar"
+    }
+},
+
+{
+    infinitive:"içmek",
+    root:"iç",
+    zh:"喝",
+
+    present:{
+        ben:"içiyorum",
+        sen:"içiyorsun",
+        o:"içiyor",
+        biz:"içiyoruz",
+        siz:"içiyorsunuz",
+        onlar:"içiyorlar"
+    }
+},
+
+{
+    infinitive:"çalışmak",
+    root:"çalış",
+    zh:"工作",
+
+    present:{
+        ben:"çalışıyorum",
+        sen:"çalışıyorsun",
+        o:"çalışıyor",
+        biz:"çalışıyoruz",
+        siz:"çalışıyorsunuz",
+        onlar:"çalışıyorlar"
+    }
+}
+
+];
