77cbd8a73cf8c1bd5ca033cbfb5a51ac61e10a23
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:06:30 +0300

    Create listening_quiz.js

---
+++ js/listening_quiz.js
@@ -0,0 +1,53 @@
+function listeningQuiz(){
+
+
+let item=
+
+listeningDatabase[0];
+
+
+
+let answer=
+
+prompt(
+
+"你聽到的是什麼意思？"
+
+);
+
+
+
+if(
+
+answer.includes(
+
+"袋"
+
+)
+
+){
+
+
+alert(
+
+"🎉 正確"
+
+);
+
+
+}
+
+else{
+
+
+alert(
+
+"再聽一次"
+
+);
+
+
+}
+
+
+}
