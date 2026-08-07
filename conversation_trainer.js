6bf20ec1b3e433567b626e0ed1c28b0794adcfd9
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:48:14 +0300

    Create conversation_trainer.js

---
+++ conversation_trainer.js
@@ -0,0 +1,63 @@
+const introduction=[
+
+
+{
+
+tr:
+"Merhaba, ben Eva.",
+
+zh:
+"你好，我是Eva。"
+
+},
+
+
+
+{
+
+tr:
+"Ben Tayvanlıyım.",
+
+zh:
+"我是台灣人。"
+
+},
+
+
+
+{
+
+tr:
+"İstanbul'da yaşıyorum.",
+
+zh:
+"我住在伊斯坦堡。"
+
+},
+
+
+
+{
+
+tr:
+"Türkçe öğreniyorum.",
+
+zh:
+"我正在學土耳其語。"
+
+},
+
+
+
+{
+
+tr:
+"Türkçem biraz kötü.",
+
+zh:
+"我的土耳其語不太好。"
+
+}
+
+
+];
