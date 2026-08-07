5bfe8505712896bd96f5d45a91f61c4e89452c21
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 12:15:45 +0300

    Create a1_30days.js

---
+++ js/a1_30days.js
@@ -0,0 +1,191 @@
+const a1ThirtyDays=[
+
+
+{
+day:1,
+
+title:"你好與自我介紹",
+
+words:[
+"Merhaba 你好",
+"Ad 我的名字",
+"Ben 我",
+"Sen 你",
+"Türkçe 土耳其語"
+],
+
+task:
+"完成30秒自我介紹"
+
+},
+
+
+
+{
+day:2,
+
+title:"國籍與語言",
+
+words:[
+"Türk 台湾",
+"Çin 中國",
+"Tayvan 台灣",
+"İngilizce 英文",
+"Türkçe 土耳其語"
+],
+
+task:
+"說出我是台灣人"
+},
+
+
+
+{
+day:3,
+
+title:"數字與價格",
+
+words:[
+"Bir 一",
+"On 十",
+"Yüz 百",
+"Bin 千",
+"Kaç para 多少錢"
+],
+
+task:
+"在超市問價格"
+
+},
+
+
+
+{
+day:4,
+
+title:"購物",
+
+words:[
+"Almak 買",
+"Vermek 給",
+"Poşet 袋子",
+"Kasa 收銀台"
+],
+
+task:
+"完成超市結帳"
+},
+
+
+
+{
+day:5,
+
+title:"咖啡店",
+
+words:[
+"Kahve 咖啡",
+"Su 水",
+"İstiyorum 我想要",
+"Lütfen 請"
+],
+
+task:
+"點一杯咖啡"
+
+},
+
+
+
+{
+day:6,
+
+title:"家與公寓",
+
+words:[
+"Ev 家",
+"Apartman 公寓",
+"Aidat 管理費",
+"Komşu 鄰居"
+],
+
+task:
+"跟鄰居打招呼"
+
+},
+
+
+
+{
+day:7,
+
+title:"交通",
+
+words:[
+"Metro 地鐵",
+"Otobüs 公車",
+"Durak 車站",
+"Bilet 車票"
+],
+
+task:
+"問路"
+
+},
+
+
+
+{
+day:8,
+
+title:"方向",
+
+words:[
+"Sağ 右",
+"Sol 左",
+"Düz 直走",
+"Yakın 附近"
+],
+
+task:
+"找到目的地"
+
+},
+
+
+
+{
+day:9,
+
+title:"時間",
+
+words:[
+"Saat 時間",
+"Bugün 今天",
+"Yarın 明天",
+"Şimdi 現在"
+],
+
+task:
+"約時間"
+
+
+},
+
+
+
+{
+day:10,
+
+title:"現在式",
+
+grammar:
+"Ben + verb + yor",
+
+task:
+"說自己的日常"
+
+}
+
+
+];
