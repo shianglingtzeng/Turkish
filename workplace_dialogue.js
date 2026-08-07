94443493fed22b2ac710ffb47d0d3ba9e730c727
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:48:29 +0300

    Create workplace_dialogue.js

---
+++ workplace_dialogue.js
@@ -0,0 +1,55 @@
+const workplaceDialogue=[
+
+
+{
+
+
+tr:
+"Toplantı ne zaman?",
+
+zh:
+"會議什麼時候？"
+
+},
+
+
+
+{
+
+
+tr:
+"Dosyayı gönderdim.",
+
+zh:
+"我已經寄文件。"
+
+},
+
+
+
+{
+
+
+tr:
+"Kontrol eder misiniz?",
+
+zh:
+"可以幫忙確認嗎？"
+
+},
+
+
+
+{
+
+
+tr:
+"Bir dakika lütfen.",
+
+zh:
+"請等一下。"
+
+}
+
+
+];
