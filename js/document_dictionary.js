201833190d2eddd18ae063b5b024349d0458cf54
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:10:51 +0300

    Create document_dictionary.js

---
+++ js/document_dictionary.js
@@ -0,0 +1,130 @@
+const governmentDictionary=[
+
+
+{
+
+tr:"Adres Kaydı",
+
+pronunciation:"阿德雷斯 卡伊德",
+
+zh:"地址登記",
+
+category:"居住",
+
+explain:
+"外國人在土耳其常需要辦理的住址登記"
+
+},
+
+
+
+{
+
+tr:"İkamet İzni",
+
+pronunciation:"伊卡梅特 伊茲尼",
+
+zh:"居留許可",
+
+category:"移民",
+
+explain:
+"外國人在土耳其合法居留文件"
+
+},
+
+
+
+{
+
+tr:"Çalışma İzni",
+
+pronunciation:"恰勒什瑪 伊茲尼",
+
+zh:"工作許可",
+
+category:"工作",
+
+explain:
+"外國人在土耳其工作的許可文件"
+
+},
+
+
+
+{
+
+tr:"T.C. Kimlik No",
+
+pronunciation:
+"特謝 金利克 諾",
+
+zh:
+"土耳其身分識別號",
+
+category:"身份",
+
+explain:
+"官方表格、銀行、政府常使用"
+
+},
+
+
+
+{
+
+tr:"Vergi Numarası",
+
+pronunciation:
+"維爾吉 努馬拉瑟",
+
+zh:
+"稅號",
+
+category:"財務",
+
+explain:
+"銀行或商業流程可能需要"
+
+},
+
+
+
+{
+
+tr:"Kira Sözleşmesi",
+
+pronunciation:
+"奇拉 瑟茲雷什梅",
+
+zh:
+"租約",
+
+category:"住房",
+
+explain:
+"證明居住地址的重要文件"
+
+},
+
+
+
+{
+
+tr:"Noter",
+
+pronunciation:
+"諾特",
+
+zh:
+"公證處",
+
+category:"文件",
+
+explain:
+"文件認證機構"
+
+}
+
+
+];
