1f83b40b1799f693bdcd4e63fd65695e9b4d7307
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 11:30:02 +0300

    Update review.js

--- review.js
+++ review.js
@@ -1,1 +1,54 @@
+function getReviewWords(){
 
+
+let review=[];
+
+
+
+learnedWords.forEach(word=>{
+
+
+if(
+Math.random()>0.5
+){
+
+review.push(word);
+
+}
+
+
+});
+
+
+return review;
+
+
+}
+
+
+
+function startReview(){
+
+
+let words=
+
+getReviewWords();
+
+
+
+alert(
+
+"今日複習："
+
++
+
+words.length
+
++
+
+" 個單字"
+
+);
+
+
+}
