0885101d0df0f03db7260d4d65eab88125ef4c7e
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 12:14:13 +0300

    Create a1_course.js

---
+++ js/a1_course.js
@@ -0,0 +1,145 @@
+const a1Course=[
+
+
+{
+
+lesson:1,
+
+title:"你好與自我介紹",
+
+topics:[
+
+"Merhaba",
+
+"Benim adım...",
+
+"我是誰",
+
+"國籍",
+
+"職業"
+
+],
+
+
+sentences:[
+
+
+{
+tr:"Benim adım Eva.",
+zh:"我的名字是Eva"
+},
+
+
+{
+tr:"Ben Tayvanlıyım.",
+zh:"我是台灣人"
+},
+
+
+{
+tr:"İstanbul'da yaşıyorum.",
+zh:"我住在伊斯坦堡"
+}
+
+
+]
+
+
+},
+
+
+
+
+{
+
+lesson:2,
+
+title:"基本問句",
+
+topics:[
+
+"Ne",
+
+"Nerede",
+
+"Kim",
+
+"Nasıl",
+
+"Kaç"
+
+],
+
+
+sentences:[
+
+
+{
+tr:"Bu ne?",
+zh:"這是什麼？"
+},
+
+
+{
+tr:"Nerede?",
+zh:"在哪裡？"
+},
+
+
+{
+tr:"Kaç para?",
+zh:"多少錢？"
+}
+
+
+]
+
+
+},
+
+
+
+
+{
+
+lesson:3,
+
+title:"日常動詞",
+
+topics:[
+
+"gitmek 去",
+
+"gelmek 來",
+
+"istemek 想要",
+
+"almak 買"
+
+],
+
+
+sentences:[
+
+
+{
+tr:"Su almak istiyorum.",
+zh:"我想買水"
+},
+
+
+{
+tr:"Metroya gidiyorum.",
+zh:"我要去地鐵"
+}
+
+
+]
+
+
+}
+
+
+
+];
