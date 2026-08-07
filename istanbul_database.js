a948208af629772c33c7cc29047b51fad1bd91c0
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 10:50:54 +0300

    Update istanbul_database.js

--- istanbul_database.js
+++ istanbul_database.js
@@ -1,1 +1,80 @@
+const istanbulDatabase=[
 
+
+{
+word:"Son Kullanma Tarihi",
+
+meaning:"有效期限",
+
+sound:"Son Kul-lan-ma Ta-ri-hi",
+
+place:"食品包裝",
+
+sentence:
+"Tarihi geçmiş mi?",
+
+translation:
+"過期了嗎？"
+
+},
+
+
+
+{
+word:"İndirim",
+
+meaning:"折扣",
+
+sound:"因迪林",
+
+place:"超市/商場",
+
+sentence:
+"İndirim var mı?",
+
+translation:
+"有折扣嗎？"
+
+},
+
+
+
+{
+word:"Fiyat",
+
+meaning:"價格",
+
+sound:"菲亞特",
+
+place:"商品標籤",
+
+sentence:
+"Fiyat ne kadar?",
+
+translation:
+"多少錢？"
+
+},
+
+
+
+{
+word:"İçindekiler",
+
+meaning:"成分",
+
+sound:"伊欽德基勒",
+
+place:"食品",
+
+sentence:
+"İçindekiler nerede?",
+
+translation:
+"成分在哪？"
+
+}
+
+
+
+];
