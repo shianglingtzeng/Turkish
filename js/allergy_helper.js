a61e519488b2a5be3b1f90c229a3376043c96d08
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:43:47 +0300

    Create allergy_helper.js

---
+++ js/allergy_helper.js
@@ -0,0 +1,47 @@
+const allergyPhrase=[
+
+
+{
+
+zh:"我不吃豬肉",
+
+tr:
+"Domuz eti yemiyorum."
+
+},
+
+
+
+{
+
+zh:"我不吃辣",
+
+tr:
+"Acı yemiyorum."
+
+},
+
+
+
+{
+
+zh:"我對堅果過敏",
+
+tr:
+"Kuruyemiş alerjim var."
+
+},
+
+
+
+{
+
+zh:"我是素食者",
+
+tr:
+"Ben vejetaryenim."
+
+}
+
+
+];
