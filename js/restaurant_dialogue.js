3f15c5f8761abbd98fc76c311ff261358a09cebe
shianglingtzeng <62326709+shianglingtzeng@users.noreply.github.com>
2026-08-07 14:43:26 +0300

    Create restaurant_dialogue.js

---
+++ js/restaurant_dialogue.js
@@ -0,0 +1,61 @@
+const restaurantDialogue=[
+
+
+{
+
+role:"服務生",
+
+tr:
+"Ne istersiniz?",
+
+zh:
+"您想要什麼？"
+
+
+},
+
+
+
+{
+
+role:"服務生",
+
+tr:
+"İçecek ister misiniz?",
+
+zh:
+"需要飲料嗎？"
+
+
+},
+
+
+
+{
+
+role:"服務生",
+
+tr:
+"Afiyet olsun.",
+
+zh:
+"請享用。"
+
+},
+
+
+
+{
+
+role:"客人",
+
+tr:
+"Çok güzel.",
+
+zh:
+"很好吃。"
+
+}
+
+
+];
